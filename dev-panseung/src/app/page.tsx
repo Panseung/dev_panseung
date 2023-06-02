a5596ecbe8c86d074b78ac5f06659cdbebda5586
jodie9596@gmail.com <jodie9596@gmail.com>
2023-06-02 13:16:23 +0900

    [DP-8] 메인 헤더부분 작업

--- dev-panseung/src/app/page.tsx
+++ dev-panseung/src/app/page.tsx
@@ -1,6 +1,21 @@
+import styles from './page.module.scss'
+
 export default function Home() {
   return (
-    <div>
+    <div className={styles['main-wrapper']}>
+      <div className={styles['main-header']}>
+        <div className={styles['header-title']}>
+          Hi, I'm seunghwan Kim🖐️
+        </div> 
+        <div className={styles['header-content']}>
+          I'm a Junior Front-end Engineer.
+        </div>
+        <div className={styles['header-content']}>
+        I worked for 6 months at a POS service company with Vue2,
+        <br></br>
+        and made a product sales statistics service.📊
+        </div>
+      </div>
     </div>
   )
 }
