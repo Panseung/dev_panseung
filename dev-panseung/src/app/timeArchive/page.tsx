04d282d55142ae3a1c3e71de1db97efc2f0c6f7d
jodie9596@gmail.com <jodie9596@gmail.com>
2023-06-05 19:59:43 +0900

    [DP-9] title 변경

--- dev-panseung/src/app/timeArchive/page.tsx
+++ dev-panseung/src/app/timeArchive/page.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import styles from './page.module.scss'
 
 export default function timeArchive() {
   return (
+    <>
+    <Head>
+      <title>hoih</title>
+    </Head>
     <div className={styles['time-archive-wrapper']}>
       여기는 타임 아카이브
     </div>
+    </>
   )
 }
