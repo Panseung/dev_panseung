94a7b9f1a4f77af0feac610d8d2a6f3c64634175
jodie9596@gmail.com <jodie9596@gmail.com>
2023-07-03 21:54:11 +0900

    [DP-13] 기본 퍼블리싱 구현

--- dev-panseung/src/app/devWriting/page.tsx
+++ dev-panseung/src/app/devWriting/page.tsx
@@ -1,9 +1,41 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './page.module.scss'
+import moment from 'moment'
 
 export default function DevWriting() {
+  
+  const datas = [
+    { title: '테스트 타이틀 1',
+      content: '테스트 내용 1',
+      time: moment().calendar() }, 
+    { title: '테스트 타이틀 2',
+      content: '테스트 내용 2',
+      time: moment().format("MMM Do YY") }, 
+  ]
+
   return (
     <div className={styles['dev-writing-wrapper']}>
-      여기는 데브라이팅
+      <div className={styles['writing-header']}>
+        <div className={styles['title']}>
+          Dev Writing
+        </div> 
+        <div className={styles['content']}>
+          I mainly write about development learning
+        </div> 
+      </div>
+      <div className={styles['writing-box']}>
+        { datas.map( function( data, i ) {
+          return (
+          <div className={styles['writing-item']} key={i}>
+            <div className={styles['item-title']}>{ data.title }</div>
+            <div className={styles['item-content']}>{ data.content }</div>
+            <div className={styles['item-time']}>{ data.time }</div>
+          </div>
+          )
+        } ) }
+      </div>
     </div>
   )
 }
