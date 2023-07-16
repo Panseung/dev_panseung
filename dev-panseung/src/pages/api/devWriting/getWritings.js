b3b495d9acebcd4edc8acc974a018132e329070a
jodie9596@gmail.com <jodie9596@gmail.com>
2023-07-17 07:48:45 +0900

    [DP-13] isDeleted 칼럼 추가 및 삭제되지 않은 데이터만 가져오도록 쿼리 수정

--- dev-panseung/src/pages/api/devWriting/getWritings.js
+++ dev-panseung/src/pages/api/devWriting/getWritings.js
@@ -2,7 +2,7 @@ import dbConnection from '../../../util/db'
 
 export default async function handler(req,res) {
   try {
-    const writings = await dbConnection.promise().query('SELECT * FROM \`dev-Panseung\`.dev_writing')
+    const writings = await dbConnection.promise().query('SELECT * FROM \`dev-Panseung\`.dev_writing WHERE not isDeleted')
     res.status(200).json(writings[0])
   } catch (error) {
     console.error(error)
