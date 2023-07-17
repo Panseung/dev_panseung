9b8999a9a2c63cbea656e43297315aafe1171b8a
jodie9596@gmail.com <jodie9596@gmail.com>
2023-07-17 10:11:33 +0900

    [DP-13] 데이터 파싱 및 기본 퍼블리싱

--- dev-panseung/src/app/devWriting/page.tsx
+++ dev-panseung/src/app/devWriting/page.tsx
@@ -1,29 +1,46 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import _ from 'lodash'
 import styles from './page.module.scss'
 import { useRouter } from 'next/navigation'
+import moment from 'moment'
+
+type Data = {
+  id: number
+  title: string
+  content: string
+  created_time: string
+  comment_count: number
+}
 
 export default function DevWriting() {
 
-  const isManager :boolean = useSelector((state :object) => _.get(state, 'isManager') || false )
+  const isManager :boolean = useSelector((state: object) => _.get(state, 'isManager') || false )
 
-  const [datas, setDatas] = useState([])
+  const [datas, setDatas] = useState<Data[]>([])
 
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const fetchData = async function() {
+  const fetchData = useCallback(async () => {
     try {
       const response = await fetch('/api/devWriting/getWritings')
       const result = await response.json()
-      setDatas(result)
+      const parsedData = dataParse(result)
+      setDatas(parsedData)
     } catch (error) {
       console.error(error)
     }
+  }, [])
+
+  const dataParse = function(rowDatas: object) {
+    return _.map( rowDatas, (data, i) => {
+      const id: number = _.get(data, 'id') || 99999 + i
+      const title: string = _.get(data, 'title') || '제목을 불러오지 못했습니다.'
+      const content: string = _.get(data, 'content') || '글 내용을 불러오지 못했습니다.'
+      const created_time: string = moment(_.get(data, 'created_time')).format('YYYY-MM-DD') || '9999-99-99'
+      const comment_count: number = _.get(data, 'comment_count') || 0
+      return { id, title, content, created_time, comment_count }
+    } )
   }
 
   const showDeleteAlarm = function( id: number ) {
@@ -49,6 +66,10 @@ export default function DevWriting() {
     router.push('/devWriting/write')
   }
 
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className={styles['dev-writing-wrapper']}>
       <div className={styles['writing-header']}>
@@ -65,17 +86,17 @@ export default function DevWriting() {
         </div>
       </div>
       <div className={styles['writing-box']}>
-        { datas.length > 0 && datas.map( function( data, i ) {  // 추후에 로대시 사용해서 변경
+        { datas.length > 0 && datas.map( function( data ) {  // 추후에 로대시 사용해서 변경
           return (
-          <div className={styles['writing-item']} key={i}>
+          <div className={styles['writing-item']} key={data.id}>
             <div className={styles['upper-area']}>
-              <div className={styles['item-title']}>{ _.get(data, 'title') }</div>
+              <div className={styles['item-title']}>{ data.title }</div>
               {isManager && (
-                <button className={styles['delete-btn']} onClick={() => {showDeleteAlarm(_.get(data, 'id'))}}>글삭제</button>
+                <button className={styles['delete-btn']} onClick={() => {showDeleteAlarm(data.id)}}>글삭제</button>
               )}
             </div>
-            <div className={styles['item-content']}>{ _.get(data, 'content') }</div>
-            <div className={styles['item-time']}>{ _.get(data, 'created_time') }</div>
+            <div className={styles['item-content']}>{ data.content }</div>
+            <div className={styles['item-time']}>{ data.created_time }</div>
           </div>
           )
         } ) }
--- dev-panseung/src/util/db.ts
+++ dev-panseung/src/util/db.ts
@@ -1,9 +1,10 @@
-import { createConnection } from 'mysql2'
-const dbConnection = createConnection({
+import { createPool  } from 'mysql2'
+const dbConnection = createPool ({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectionLimit: 5
 });
 
 export default dbConnection;
